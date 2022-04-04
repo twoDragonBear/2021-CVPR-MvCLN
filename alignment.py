6a56d61d7f5bc8c6d08f55b2d13c84606ce08362
twoDragonBear <553435597@qq.com>
2022-04-04 15:03:28 +0800

    update: eliminate deprecated code

--- alignment.py
+++ alignment.py
@@ -53,7 +53,7 @@ def euclidean_dist(x, y):
     yy = torch.pow(y, 2).sum(1, keepdim=True).expand(n, m).t()
     dist = xx + yy
     # torch.addmm(beta=1, input, alpha=1, mat1, mat2, out=None)，这行表示的意思是dist - 2 * x * yT
-    dist.addmm_(1, -2, x, y.t())
+    dist.addmm_(x, y.t(), beta=1, alpha=-2)
     # clamp()函数可以限定dist内元素的最大最小范围，dist最后开方，得到样本之间的距离矩阵
     dist = dist.clamp(min=1e-12).sqrt()  # for numerical stability
     return dist
